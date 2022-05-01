275d8e58862b83bc7506bbe0f1777cdce17dca57
= <=>
2022-05-01 20:26:39 +0530

    Commit_1_5_22_001

--- test/PageObjects_FrontStore/FrontEnd_Reg_Page_MercuryTours.js
+++ test/PageObjects_FrontStore/FrontEnd_Reg_Page_MercuryTours.js
@@ -8,17 +8,22 @@ class Registration_MercuryTours
     get PassWord() {return $("//input[@name='password']")}
     get ConfirmPassword() {return $("//input[@name='confirmPassword']")}
     get SubmitButn() {return $("//input[@name='submit']")}
+    get RegisterLink() {return $("//a[text()='REGISTER']")}
 
     // Develop action methods
     async registerUserMercuryTours(fn, ln, country, un, pass, confpass)
     {
         await this.SubmitButn.waitForClickable()
-        await this.firstName.sendKeys(fn)
-        await this.LastName.sendKeys(ln)
+        await this.firstName.setValue(fn)
+        await this.LastName.setValue(ln)
         await this.Country.selectByVisibleText(country)
-        await this.userName.sendKeys(un)
-        await this.PassWord.sendKeys(pass)
-        await this.ConfirmPassword.sendKeys(confpass)
+        await this.userName.setValue(un)
+        await this.PassWord.setValue(pass)
+        await this.ConfirmPassword.setValue(confpass)
         await this.SubmitButn.click()
+        await this.RegisterLink.waitForDisplayed()
+        await this.RegisterLink.click()
     }
 } 
+
+module.exports = new Registration_MercuryTours()
--- test/Specs/FrontEnd_TestCase4_MercuryTours_Register.js
+++ test/Specs/FrontEnd_TestCase4_MercuryTours_Register.js
@@ -1,14 +1,24 @@
 var mercury_reg = require ('./../PageObjects_FrontStore/FrontEnd_Reg_Page_MercuryTours')
-data_base = require('./../../Utilities/Database_Connect')
+var data_base = require('./../../Utilities/Database_Connect')
+var json_data = require ('./../../TestData/DBQueryResult.json')
 
 describe("TestCase4_RegisterUser_MercuryTours_Database", ()=>{
 
       it ("Register the user Mercury Tours_Database values", async ()=>{
          await browser.maximizeWindow()
          await browser.url("https://demo.guru99.com/test/newtours/register.php")
-         await browser.pause(5000)
 
-         // Execute the Database operation
+        // await mercury_reg.registerUserMercuryTours('Ram','Chigari','CANADA','Guru','tester','tester')
+         
+
+         for (items in json_data)
+         {
+           await mercury_reg.registerUserMercuryTours(json_data[items].FirstName,json_data[items].LastName,
+               json_data[items].Country.toUpperCase(),json_data[items].Username,json_data[items].Password,json_data[items].Password)              
+               
+         }
+
+         
          
          
 
